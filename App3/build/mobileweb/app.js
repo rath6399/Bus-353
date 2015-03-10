d3caf4dabb927709a03087808ce607f04c9f7f6b
rath6399 <rath6399@vandals.uidaho.edu>
2015-03-09 22:38:55 -0700

    sync

--- App3/build/mobileweb/app.js
+++ App3/build/mobileweb/app.js
@@ -5,36 +5,68 @@
  * Spring 2015
  */
 
-(function(){
-	var win1 = Titanium.UI.createWindow({
+var pages = Ti.UI.createTabGroup();
+
+var win1 = Titanium.UI.createWindow({
 		title: 'Select Color',
 		backgroundColor: '#fff'
 	});
-	var Teas = ['#F5F5DC', '#FFE4B5', '#FFE4C4', '#D2B48C', 
+var Teas = ['#F5F5DC', '#FFE4B5', '#FFE4C4', '#D2B48C', 
 '#C3B091', '#C3B091', '#926F5B', '#80400', '#654321',
 '#3D2B1F' ];
 
-	allRows = [];
-	var theColours = Ti.UI.createTableView({});
+allRows = [];
+var theColours = Ti.UI.createTableView({});
 
-	for (var i=0; i<Teas.length; i++){
-		theRow = Ti.UI.createTableView({
-			backgroundColor: Teas[i],
-			height: 50,
-			TeaColour: Teas[i]
-		});
-		allRows.push(theRow);
+for (var i=0; i<Teas.length; i++){
+	theRow = Ti.UI.createTableView({
+		backgroundColor: Teas[i],
+		height: 50,
+		TeaColour: Teas[i]
+	});
+	allRows.push(theRow);
 };
 
-	theColours.setData(allRows);
+theColours.setData(allRows);
 	
-	theColours.addEventListener('click', function(e){
-		showTeaVerdict(e.source.TeaColour);
-	});
-	
-	win1.add(theColours);
-	win1.open;
-})();
+theColours.addEventListener('click', function(e){
+	showTeaVerdict(e.source.TeaColour);
+});
+var Tab1 = Ti.UI.createTab({
+	title:'Tea',
+	window: win1
+});
+
+var win2 = Ti.UI.createWindow({
+	backgroundColor: #fff
+});
+
+var options = TI.UI.createView({
+	layout:'vertical'
+});
+
+var showCamera = Ti.UI.createButton({
+	title: 'Show Camera'
+});
+
+var thePhoto = Ti.UI.createImageView({
+	height: '30%',
+	width: '30%'
+});
+
+var Tab2 = TI.UI.createTab({
+	title: 'Camera',
+	window: win2
+});
+
+win1.add(theColours);
+options.add(showCamera);
+options.add(thePhoto);
+win2.add(options);
+pages.add(Tab1);
+pages.add(Tab2);
+
+pages.open();
 
 
 function getVerdict(colour){
@@ -77,9 +109,17 @@ function showTeaVerdict(_args) {
 	teaVerdict.open;
 };
 
+function showPhoto(_args){
+	thePhoto.setImage(_args.media);
+}
 
-
-/*theColours.addEventListener('click', function(e){
-	showTeaVerdict(e.source.TeaColour);
-	});
-*/
+showCamera.addEventListener('click', function(e) {
+	Ti.Media.showCamera({animated: true,
+		autoHide: true,
+		saveToPhotoGallery: true,
+		showControls: true,
+		mediaTypes: [Ti.Media.MEDIA_TYPE_PHOTO],
+		success: function(e) {showPhoto(e)},
+		error: function (e) {alert('There was a problem accessing the camera')}
+	})
+});
